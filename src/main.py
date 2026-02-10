ddc5768f69bc90ba54b45abb0bf95e6000127d6d
Dramaturg1 <widowmakerNoOver@outlook.com>
2026-02-11 01:52:10 +0300

    project set up

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from PySide6.QtWidgets import QApplication
+from PySide6.QtUiTools import QUiLoader
+import sys
+
+app = QApplication(sys.argv)
+
+loader = QUiLoader()
+window = loader.load("main.ui")
+
+window.show()
+
+sys.exit(app.exec())
