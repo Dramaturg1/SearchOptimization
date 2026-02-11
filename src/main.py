c273eac8eec1bc9c39c52e7b363071a74d9ad575
Dramaturg1 <widowmakerNoOver@outlook.com>
2026-02-11 15:21:13 +0300

    independent path to ui

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 from PySide6.QtWidgets import QApplication
 from src.core.CustomLoader import CustomLoader
 import sys
+import os
 
 app = QApplication(sys.argv)
 
 loader = CustomLoader()
-window = loader.load("src/ui/main.ui")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+ui_path = os.path.join(current_dir, "ui", "main.ui")
+window = loader.load(ui_path)
 
 window.show()
 x = [1, 2, 3, 4, 5]
